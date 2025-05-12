aace533ed8c953f7614e411562d043d014c89478
costta <124215106+jvcostta@users.noreply.github.com>
2025-05-12 14:44:50 -0300

    Create 2203.py

---
+++ Exercício 4/2203.py
@@ -0,0 +1,67 @@
+class Solution:
+    def minimumWeight(self, n: int, edges: list[list[int]], src1: int, src2: int, dest: int) -> int:
+        def push_heap(heap, item):
+            heap.append(item)
+            _sift_up(heap, len(heap) - 1)
+
+        def pop_heap(heap):
+            if len(heap) == 1:
+                return heap.pop()
+            smallest = heap[0]
+            heap[0] = heap.pop()
+            _sift_down(heap, 0)
+            return smallest
+
+        def _sift_up(heap, idx):
+            parent = (idx - 1) // 2
+            while idx > 0 and heap[idx][0] < heap[parent][0]:
+                heap[idx], heap[parent] = heap[parent], heap[idx]
+                idx = parent
+                parent = (idx - 1) // 2
+
+        def _sift_down(heap, idx):
+            smallest = idx
+            left = 2 * idx + 1
+            right = 2 * idx + 2
+
+            if left < len(heap) and heap[left][0] < heap[smallest][0]:
+                smallest = left
+            if right < len(heap) and heap[right][0] < heap[smallest][0]:
+                smallest = right
+
+            if smallest != idx:
+                heap[idx], heap[smallest] = heap[smallest], heap[idx]
+                _sift_down(heap, smallest)
+
+        def dijkstra(start: int, graph: dict) -> list[int]:
+            dist = [inf] * n
+            dist[start] = 0
+            heap = []
+            push_heap(heap, (0, start))
+            while heap:
+                current_dist, u = pop_heap(heap)
+                if current_dist > dist[u]:
+                    continue
+                for v, weight in graph[u]:
+                    if dist[v] > dist[u] + weight:
+                        dist[v] = dist[u] + weight
+                        push_heap(heap, (dist[v], v))
+            return dist
+
+        graph = defaultdict(list)
+        reversed_graph = defaultdict(list)
+        for u, v, w in edges:
+            graph[u].append((v, w))
+            reversed_graph[v].append((u, w))
+
+        dist_from_src1 = dijkstra(src1, graph)
+        dist_from_src2 = dijkstra(src2, graph)
+        dist_to_dest = dijkstra(dest, reversed_graph)
+
+        min_total = inf
+        for i in range(n):
+            total = dist_from_src1[i] + dist_from_src2[i] + dist_to_dest[i]
+            if total < min_total:
+                min_total = total
+
+        return min_total if min_total != inf else -1
