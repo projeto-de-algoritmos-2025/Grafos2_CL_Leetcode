73ef58123bdde24f2170c5553e310d0dca491fc5
costta <124215106+jvcostta@users.noreply.github.com>
2025-05-12 19:26:08 -0300

    Update comment 1584.py

--- Exercício 2/1584.py
+++ Exercício 2/1584.py
@@ -1,8 +1,8 @@
 class Solution:
     def minCostConnectPoints(self, points: List[List[int]]) -> int:
         n = len(points)
-        visited = set() 
-        min_heap = [(0, 0)]  
+        visited = set() #para rastrear os pontos ja incluidos no plano
+        min_heap = [(0, 0)]  #(custo, índice do ponto), começando do ponto 
         total_cost = 0
 
         while len(visited) < n:
@@ -10,10 +10,11 @@ class Solution:
             if point in visited:
                 continue
             
-            
+            #adicionar o ponto atual ao plano
             visited.add(point)
             total_cost += cost
             
+            #adicionar todas as arestas que conectam este ponto a outros pontos ainda não visitados
             for next_point in range(n):
                 if next_point not in visited:
                     distance = abs(points[point][0] - points[next_point][0]) + abs(points[point][1] - points[next_point][1])
